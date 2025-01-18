4c14b01093e7223a106fb2beb72bb25cf167a635
Cherie Huang <cherry2017wood@gmail.com>
2025-01-18 12:39:24 -0800

    initial commit

---
+++ src/main/java/com/example/demo/NwHacks2025Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NwHacks2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NwHacks2025Application.class, args);
+	}
+
+}
