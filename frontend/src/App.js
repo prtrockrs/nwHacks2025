f7e8deec6450920095fd3cb3d5ab4dc81bfd680a
Erika <efeng.ca@gmail.com>
2025-01-18 16:44:14 -0800

    integrated react

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import MessageComponent from './components/MessageComponent';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <MessageComponent />
+            </header>
+        </div>
+    );
+}
+
+export default App;
